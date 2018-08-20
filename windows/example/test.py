175194247c587b429b931174d8e28e0c15a1946e
insigh <zhangchaojie@ruc.edu.cn>
2018-08-20 18:42:13 +0800

    recover

--- windows/example/test.py
+++ windows/example/test.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 
-src = cv.imread("/home/zcj/Pictures/8776db91659bf1b9abada9bbc9d9f15d0b085642.jpg")
+src = cv.imread("C:\\Users\zcj\Desktop\docum\photos\island.jpg")
 cv.namedWindow("input-image",cv.WINDOW_AUTOSIZE)
 cv.imshow("input-image",src)
 cv.waitKey(0)
